45fb183ff89a833fb5639ae69cb290ac661ee61b
Peter Brink <peterg@varpro.org>
2019-05-11 19:57:02 -0400

    moved maing file to top level and fixed example

--- example/server.js
+++ example/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const QuickBooks = require('../quickbooks-promise')
-const QuickBooks = require('../src')
+const QuickBooks = require('..')
 
 var jsonParser = bodyParser.json()
 
